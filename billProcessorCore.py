8dfc42a9922e205937396e9875d14d679be5d57b
Xiang Chen <diamondowen@gmail.com>
2016-10-24 21:23:44 -0700

    Delete old functions

--- billProcessorCore.py
+++ billProcessorCore.py
@@ -95,34 +95,6 @@ class BillDataProcessor:
 
         conn.close
 
-
-    # def processData(self, files):
-    #     conn = sqlite3.connect(self.sqlite_file)
-    #     c = conn.cursor()
-    #     sql = 'create table if not exists ' + self.trans_table_name + ' (date text, bank text, card text, amount real, description text)'
-    #     c.execute(sql)
-    #
-    #     for file in files:
-    #         if "Chase" in file:
-    #             self.__saveDataToDB(c, transFile)
-    #         elif "Amex" in file:
-    #             self.__processAmexData(file)
-    #         elif "BOA" in file:
-    #             self.__processBOAData(file)
-    #         else:
-    #             self.errMessage += "Non-recognized data: " + file
-    #
-    #     self.statistics = self.__getStatisticsFromData()
-    #
-    #
-    #     if(len(self.errMessage) > 0):
-    #         print self.errMessage
-    #
-    #     self.__saveDataToDB(c, self.transData)
-    #
-    #     conn.commit()
-    #     conn.close()
-
     def printStatistics(self, highlight_threshold):
         self.__getStatisticsFromData()
 
@@ -245,48 +217,6 @@ class BillDataProcessor:
                     month, date, year = getMonthDateAndYearFromDate(row[0])
                     transData.append(TransactionInfo(row[0], "Amex", card, float(row[2]), row[3] ))
         return TransactionFile(file, self.currentDate, transData)
-
-
-    def __processBOAData(self, file):
-        print "Process BOA data:", file
-        card = file.split("_")[1]
-
-        with open(file, 'r') as csvFile:
-            data = csv.DictReader(csvFile, delimiter = ',')
-            for row in data:
-                month, date, year = getMonthDateAndYearFromDate(row['Posted Date'])
-                # print month, self.month, year, self.year
-                if(month == self.month and year == self.year and str(row) not in self.trans):
-                    self.trans.add(str(row))
-                    self.transData.append((row['Posted Date'], float(row['Amount']), row['Payee'], "BOA", card))
-
-
-    def __processChaseData(self, file):
-        print "Process Chase data:", file
-        card = file.split("_")[1]
-        with open(file, 'r') as csvFile:
-            data = csv.DictReader(csvFile, delimiter = ',')
-            for row in data:
-                # print row['Type'], row['Trans Date'], row['Description'],row['Amount']
-                month, date, year = getMonthDateAndYearFromDate(row['Trans Date'])
-                if(month == self.month and year == self.year and str(row) not in self.trans):
-                    self.trans.add(str(row))
-                    self.transData.append((row['Trans Date'], float(row['Amount']), row['Description'], "Chase", card))
-
-
-    def __processAmexData(self, file):
-        print "Process Amex data:", file
-        card = file.split("_")[1]
-        with open(file, 'r') as csvFile:
-            data = csv.reader(csvFile, delimiter=',')
-            for row in data:
-                # print row
-                if(len(row) > 4):
-                    month, date, year = getMonthDateAndYearFromDate(row[0])
-                    if(month == self.month and year == self.year and str(row) not in self.trans):
-                        self.transData.append((row[0], float(row[2]), row[3], "Amex", card))
-                        self.trans.add(str(row))
-
 
     def printAllTransData(self):
         for date, amount, desc, bank, card in self.transData:
